ab715bd89bfd1d8e96b3aaa453bac4f195f0989b
Jayakanth <jayakanth1812@gmail.com>
2023-08-19 15:16:57 +0530

    formated code

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import HeadLineCards from "./components/HeadLineCards";
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Hero />
+      <HeadLineCards />
     </>
   );
 };
---
+++ src/components/HeadLineCards.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const HeadLineCards = () => {
+  return (
+    <div>
+      {/* card */}
+      <div>
+        <div>
+          <p>Sun's Out, BOGO's Out</p>
+          <p>Through 8/26</p>
+          <button>Order Now</button>
+        </div>
+        <img
+          src="https://images.pexels.com/photos/12737656/pexels-photo-12737656.jpeg?auto=compress&cs=tinysrgb&w=600"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
+
+export default HeadLineCards;
